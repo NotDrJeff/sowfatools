4e7918fd788d0eae87c515c46d9aa050d2eb84cf
NotDrJeff <NotDrJeff@gmail.com>
2023-09-01 13:43:33 +0100

    minor fixes to exampleScript.py

--- exampleScript.py
+++ exampleScript.py
@@ -1,6 +1,8 @@
 #!/bin/python3
 
 import logging
+import sys
+from pathlib import Path
 
 import constants as const
 import utils
@@ -12,10 +14,11 @@ import waketools as wake
 logger = logging.getLogger(__name__)
 
 def main():
-    utils.configure_logging(const.SOWFATOOLS_DIR / f'log.{Path(__file__).stem}',
+    utils.configure_logging(const.SOWFATOOLS_DIR/f'log.{Path(__file__).stem}',
                             level=logging.DEBUG)
     
     pass
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
+    
