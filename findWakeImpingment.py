a80e4877e1e589e043a3a75d4d4bcbbbcb1d6a90
NotDrJeff <NotDrJeff@gmail.com>
2023-09-07 11:49:17 +0100

    added findWakeImpingment.py

---
+++ findWakeImpingment.py
@@ -0,0 +1,128 @@
+#!/bin/python3
+
+import logging
+import sys
+from pathlib import Path
+
+import numpy as np
+import sympy.geometry as geom
+import paraview
+import paraview.simple as simple
+
+import constants as const
+import utils
+import pvtools as pv
+
+logger = logging.getLogger(__name__)
+
+refined_region_origin = [672.0, 806.0, 0.0]
+refined_region_dimensions = [2132.0, 368.0, 274.0]
+
+
+def main(case_name):
+    utils.configure_logging((const.CASES_DIR / case_name / const.SOWFATOOLS_DIR
+                             / f'log.{Path(__file__).stem}'),
+                            level=logging.DEBUG)
+    
+    # Use paraview to get wake outline at downstream location
+    paraview.compatibility.major = 5
+    paraview.compatibility.minor = 10
+    simple._DisableFirstRenderCameraReset()
+    
+    case_dir = const.CASES_DIR / case_name
+    fname = case_dir / f'{case_name}.foam'
+    cellarrays = ['UAvg']
+    ofcase = pv.loadof(fname,cellarrays)
+    
+    # create a new 'Extract Cells By Region'
+    extractCells = simple.ExtractCellsByRegion(Input=ofcase)
+    extractCells.IntersectWith = 'Box'
+    extractCells.IntersectWith.Position = refined_region_origin
+    extractCells.IntersectWith.Rotation = [0,0,const.WIND_DIRECTION_DEG+90]
+    extractCells.IntersectWith.Length = refined_region_dimensions
+
+    # create a new 'Ellipse'
+    ellipse = simple.Ellipse()
+    ellipse.Center = const.TURBINE_ORIGIN
+    ellipse.Normal = const.WIND_UNIT_VECTOR
+    ellipse.MajorRadiusVector = [const.TURBINE_RADIUS,0,0]
+    
+    # create a new 'Cell Data to Point Data'
+    pointdata = pv.create_cellDataToPointData(extractCells,cellarrays)
+
+    # create a new 'Stream Tracer With Custom Source'
+    streamTracer = simple.StreamTracerWithCustomSource(Input=pointdata,
+                                                       SeedSource=ellipse)
+    streamTracer.Vectors = ['POINTS'] + cellarrays
+    streamTracer.MaximumStreamlineLength = const.DOMAIN_MAXDISTANCE
+    streamTracer.IntegrationDirection = 'FORWARD'
+
+    # create a new 'Slice'
+    slice1 = pv.create_slice(streamTracer,)
+    slice1 = Slice(registrationName='Slice1', Input=streamTracer)
+    slice1.SliceType = 'Plane'
+    slice1.HyperTreeGridSlicer = 'Plane'
+    slice1.SliceOffsetValues = [0.0]
+
+    # init the 'Plane' selected for 'SliceType'
+    slice1.SliceType.Origin = [1767.273193359375, 1664.7089233398438, 105.95406341552734]
+
+    # init the 'Plane' selected for 'HyperTreeGridSlicer'
+    slice1.HyperTreeGridSlicer.Origin = [1767.273193359375, 1664.7089233398438, 105.95406341552734]
+
+    # Properties modified on slice1.SliceType
+    slice1.SliceType.Origin = [1882.0, 1721.0, 90.0]
+    slice1.SliceType.Normal = [0.8, 0.5, 0.0]
+
+    UpdatePipeline(time=21000.0, proxy=slice1)
+
+    # save data
+    SaveData('/mnt/scratch2/users/40146600/t006/sowfatools/streamLines/t006_streamLines_turbine0_foward_intersect.csv', proxy=slice1, PointDataArrays=['AngularVelocity', 'IntegrationTime', 'Rotation', 'UAvg', 'Vorticity'],
+        CellDataArrays=['ReasonForTermination', 'SeedIds'],
+        FieldDataArrays=['CasePath'])
+    
+    ###########################################################################
+    
+    dir = const.CASES_DIR / case_name / const.SOWFATOOLS_DIR / 'streamLines'
+    fname = dir / f'{case_name}_streamLines_turbine0_foward_intersect.csv'
+    logger.debug(f'Reading file {fname}')
+    data = np.genfromtxt(fname,names=True, delimiter=',')
+    
+    logger.debug(f'Rotating Point Coordinates unto plane normal to x axis')
+    points = list(zip(data['Points0'],data['Points1'],data['Points2']))
+    points = const.WIND_ROTATION.apply(points)
+    x = [i[0] for i in points]
+    logger.debug(f'New x values: Max={max(x)} Min={min(x)} Diff={max(x)-min(x)}')
+    points = np.array([i[1:] for i in points])
+    
+    # downstream_wake_impingement = geom.Polygon(*points)
+    # downstream_turbine = geom.Circle(const.TURBINE_ORIGIN_ROTATED[1:],
+    #                                  const.TURBINE_RADIUS)
+    # intersect = geom.intersection(downstream_turbine,
+    #                               downstream_wake_impingement)
+    
+    import pdb; pdb.set_trace()
+    
+    # create 2d cartestian grid based on number of wake sample points and
+    
+    c = 2*np.pi*const.TURBINE_RADIUS # circumference of turbine
+    n = len(points) # Number of sample points
+    # Grid spacing should be significantly larger than spacing
+    # between sample points. Here 10 times bigger.
+    grid_spacing =  10*c/n
+    
+    # bounding box
+    turbineMaxX = const.TURBINE_ORIGIN_ROTATED[1] + const.TURBINE_RADIUS
+    turbineMinX = const.TURBINE_ORIGIN_ROTATED[1] - const.TURBINE_RADIUS
+    maxX = max(np.max(points[:,0]),turbineMaxX)
+    minX = min(np.min(points[:,0]),turbineMinX)
+    
+    np.linspace(0,)
+    
+    np.meshgrid()
+    
+    
+
+if __name__ == "__main__":
+    main(*sys.argv[1:])
+    
