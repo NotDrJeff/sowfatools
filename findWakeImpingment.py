4136a8da7f396b81f4f5db1c54064b16aa70ae1e
NotDrJeff <NotDrJeff@gmail.com>
2023-10-04 18:36:37 +0100

    updated findWakeImpingment.py

--- findWakeImpingment.py
+++ findWakeImpingment.py
@@ -23,27 +23,26 @@ def main(casename):
     casedir = const.CASES_DIR / casename
     fname = casedir / f'{casename}.foam'
     
-    pv.calculateDownstreamWakeLocation(casename,fname,casedir)
+    # pv.calculateDownstreamWakeLocation(casename,fname,casedir)
     
     ###########################################################################
     
-    logger.debug(f'Reading file {fname}')
+    resultsdir = casedir / const.STREAMLINES_DIR
+    fname = (resultsdir
+             / f'{casename}_streamLines_turbine0_forward_intersect_turbine1.csv')
+    logger.info(f'Reading file {fname}')
     data = np.genfromtxt(fname,names=True, delimiter=',')
     
-    logger.debug(f'Rotating Point Coordinates unto plane normal to x axis')
+    logger.info(f'Rotating Point Coordinates unto plane normal to x axis')
     points = list(zip(data['Points0'],data['Points1'],data['Points2']))
     points = const.WIND_ROTATION.apply(points)
-    x = [i[0] for i in points]
-    logger.debug(f'New x values: Max={max(x)} Min={min(x)} Diff={max(x)-min(x)}')
-    points = np.array([i[1:] for i in points])
+    points = np.array([i[1:]-const.TURBINE_ORIGIN_ROTATED[1:] for i in points])
     
     # downstream_wake_impingement = geom.Polygon(*points)
     # downstream_turbine = geom.Circle(const.TURBINE_ORIGIN_ROTATED[1:],
     #                                  const.TURBINE_RADIUS)
     # intersect = geom.intersection(downstream_turbine,
     #                               downstream_wake_impingement)
-    
-    import pdb; pdb.set_trace()
     
     # create 2d cartestian grid based on number of wake sample points and
     
@@ -54,14 +53,66 @@ def main(casename):
     grid_spacing =  10*c/n
     
     # bounding box
-    turbineMaxX = const.TURBINE_ORIGIN_ROTATED[1] + const.TURBINE_RADIUS
-    turbineMinX = const.TURBINE_ORIGIN_ROTATED[1] - const.TURBINE_RADIUS
-    maxX = max(np.max(points[:,0]),turbineMaxX)
-    minX = min(np.min(points[:,0]),turbineMinX)
+    maxX = max(np.max(points[:,0]),const.TURBINE_RADIUS)
+    minX = min(np.min(points[:,0]),-const.TURBINE_RADIUS)
+    maxY = max(np.max(points[:,1]),const.TURBINE_RADIUS)
+    minY = min(np.min(points[:,1]),-const.TURBINE_RADIUS)
     
-    np.linspace(0,)
+    x = np.arange(minX,maxX+grid_spacing,grid_spacing)
+    y = np.arange(minY,maxY+grid_spacing,grid_spacing)
     
-    np.meshgrid()
+    xx,yy = np.meshgrid(x,y)
+    
+    distance_from_hub = np.sqrt(xx**2+yy**2)
+    inrotor = (distance_from_hub < const.TURBINE_RADIUS)
+    
+    # find cells where points are located
+    aroundwake = np.zeros_like(xx)
+    for point in points:
+        col = np.argmin(np.abs(x-point[0]))
+        row = np.argmin(np.abs(y-point[1]))
+        aroundwake[row,col] = 1
+        
+    # loop through each row of mesh
+    inwake = np.zeros_like(xx)
+    for row in range(inwake.shape[0]):
+        value = 0
+        for col in range(inwake.shape[1]):
+            if (aroundwake[row,col] == 1) :
+                if col == 0:
+                    value = 0 if value==1 else 1
+                elif (aroundwake[row,col-1] != 1):
+                    value = 0 if value==1 else 1
+                
+            inwake[row,col] = value
+    
+    logger.info("Plotting")
+    
+    import matplotlib.pyplot as plt
+    plt.ioff()
+    
+    # plt.pcolormesh(xx,yy,inrotor,cmap='Paired')
+    
+    # plt.plot(points[:,0],points[:,1], '.', color='r', markersize=0.5)
+    
+    # plt.pcolormesh(xx,yy,aroundwake,cmap='Paired')
+    
+    plt.pcolormesh(xx,yy,inwake,cmap='Paired')
+    
+    plt.xlim(minX,maxX)
+    plt.ylim(minY,maxY)
+    ax = plt.gca()
+    ax.set_aspect('equal', adjustable='box')
+    
+    from matplotlib.ticker import MultipleLocator
+    minorlocator = MultipleLocator(grid_spacing)
+    ax.yaxis.set_minor_locator(minorlocator)
+    ax.xaxis.set_minor_locator(minorlocator)
+    #plt.grid(which='minor')
+    
+    plt.savefig('test.png',dpi=600)
+    
+    # # import pdb; pdb.set_trace()
     
     logger.info("Finished")
     
