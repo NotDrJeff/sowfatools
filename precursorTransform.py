3735e4bd80c1b098c4eb905dce7db1b2a00c4e4e
Jeffrey Johnston <NotDrJeff@gmail.com>
2024-04-04 14:28:48 +0100

    Small changes to precursorTransform

--- precursorTransform.py
+++ precursorTransform.py
@@ -1,12 +1,15 @@
 #!/bin/python3
-"""Written for Python 3.12 as part of github.com/NotDrJeff/sowfatools
-Jeffrey Johnston   NotDrJeff@gmail.com  March 2024
+"""Written for Python 3.12, SOWFA 2.4.x
+Part of github.com/NotDrJeff/sowfatools
+Jeffrey Johnston    NotDrJeff@gmail.com     March 2024
 
 Transforms vector quantities from SOWFA precursor averaging data into
-streamwise and cross stream components"""
+streamwise and cross stream components, and calculates magnitude.
+Takes a list of cases as command line arguments.
+"""
 
 import logging
-LEVEL = logging.DEBUG
+LEVEL = logging.INFO
 logger = logging.getLogger(__name__)
 
 import argparse
@@ -24,46 +27,61 @@ def precursorTransform(casename, overwrite=False):
     
     casedir = const.CASES_DIR / casename
     sowfatoolsdir = casedir / const.SOWFATOOLS_DIR
-    avgdir = sowfatoolsdir / 'averaging'
+    avgdir = sowfatoolsdir / 'averaging'  # Same directory for read and write
     
     if not avgdir.is_dir():
-        logger.warning(f'{avgdir} directory does not exist. Skipping.')
+        logger.warning(f'{avgdir} directory does not exist. '
+                       f'Skipping {casename}.')
         return
     
-    filename = 'log.precursorTransform'
-    utils.configure_function_logger(sowfatoolsdir/filename, level=LEVEL)
+    logfilename = 'log.precursorTransform'
+    utils.configure_function_logger(sowfatoolsdir/logfilename, level=LEVEL)
     
     ############################################################################
 
-    logger.info(f'Transforming averaging for {casename}')
-    
-    with gzip.open(avgdir/f'{casename}_U_mean.gz',mode='rt') as file:
-        header = file.readline()
+    logger.info(f'Transforming vectors for {casename}')
     
     QUANTITIES = {'U_mean'  : ('U_mean',  'V_mean',  'W_mean'),
                   'q_mean'  : ('q1_mean', 'q2_mean', 'q3_mean'),
                   'Tu_mean' : ('Tu_mean', 'Tv_mean', 'Tw_mean')}
-
-    SUFFIXES = ('sw','cs')
     
+    header_found = False
+    for components in QUANTITIES.values():
+        for component in components:
+            readfile = avgdir/f'{casename}_{component}.gz'
+            if readfile.is_file():
+                with gzip.open(readfile,mode='rt') as f:
+                    header = f.readline()
+                    logger.debug(f'Using header from file {readfile}')
+                    logger.debug(f'{header=}')
+                header_found = True
+                break
+        if header_found: break
+            
+    if 'header' not in locals():
+        logger.error('No file could be found to supply a header. Exiting.')
+        raise FileNotFoundError(f'No relevant files found for case {casename}')
+    
+    header = header.removeprefix('# ').removesuffix('\n')
+            
     ############################################################################
     
-    for quantity, components in QUANTITIES.items():
-        
+    for quantity,components in QUANTITIES.items():
         logger.info(f'Processing {quantity} for {casename}')
         
         outputfiles = [(avgdir / f'{casename}_{quantity}_{suffix}.gz')
-                       for suffix in SUFFIXES]
+                       for suffix in ('sw', 'cs', 'mag')]
         
-        if all([outputfile.exists() for outputfile in outputfiles]):
-            logger.warning(f'Files already exist. Skippping.')
+        if ( all([outputfile.exists() for outputfile in outputfiles])
+             and overwrite is False ):
+            logger.warning(f'Files already exist. Skippping {quantity}.')
             continue
         
         for i, component in enumerate(components):
-            fname = avgdir / f'{casename}_{component}.gz'
-            logger.debug(f'Reading {fname}')
-            rawdata = np.genfromtxt(fname)
-
+            readfile = avgdir / f'{casename}_{component}.gz'
+            logger.debug(f'Reading {readfile}')
+            rawdata = np.loadtxt(readfile)
+            
             if i == 0:
                 data = np.empty((*rawdata.shape,3))
 
@@ -73,6 +91,9 @@ def precursorTransform(casename, overwrite=False):
         logger.debug('Transforming vectors')
         for j in range(2,data.shape[1]):
             data[:,j,:] = const.WIND_ROTATION.apply(data[:,j,:])
+            
+            # Replace vertical component with magnitude
+            data[:,j,2] = np.linalg.norm(data[:,j,:],axis=-1)
             
         for i, outputfile in enumerate(outputfiles):
             logger.debug(f'Saving file {outputfile.name}')
@@ -93,15 +114,16 @@ def precursorTransform(casename, overwrite=False):
     #             wvv_mean    wvw_mean
     #                         www_mean
 
-    logger.info(f'Finished transforming averaging for case {casename}')
+    logger.info(f'Finished transforming vectors for case {casename}')
 
 
 ################################################################################
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     utils.configure_root_logger(level=LEVEL)
 
-    description = """Calculate Streamwise and Cross-stream Components"""
+    description = """Calculate streamwise and cross-stream components of vector
+                     quantities and their magnitude"""
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('cases', help='list of cases to perform analysis for',
                         nargs='+')
