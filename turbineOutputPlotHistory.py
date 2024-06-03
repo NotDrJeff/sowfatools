a12e07ebf5f96b1477d2a15db35753df6b92b2fc
Jeffrey Johnston <NotDrJeff@gmail.com>
2024-06-03 11:58:31 +0100

    updates to script complete

--- turbineOutputPlotHistory.py
+++ turbineOutputPlotHistory.py
@@ -5,13 +5,11 @@ LEVEL = logging.DEBUG
 logger = logging.getLogger(__name__)
 
 import argparse
-import sys
-from pathlib import Path
 
 import numpy as np
 logging.getLogger('matplotlib').setLevel(logging.WARNING)
 import matplotlib.pyplot as plt
-            
+
 import constants as const
 import utils
 
@@ -48,84 +46,81 @@ def turbineOutputPlotHistory(casename, overwrite=False):
     logger.info(f'Found {len(files)} files')
     logger.info('')
     
-    ############################################################################
-    
-    filenames_parsed = []
-    for file in files:
+    filenames_parsed = [''] * len(files)
+    for i,file in enumerate(files):
+        
+        filenames_parsed[i] = file.stem.replace('turbine','')
         
         if 'blade' in file.stem:
-            filename_parsed = filename_parsed.replace('blade','')
+            filenames_parsed[i] = filenames_parsed[i].replace('blade','')
             
-        filename_parsed = file.stem.replace('turbine','')
-        filename_parsed = filename_parsed.split('_')
+        filenames_parsed[i] = filenames_parsed[i].split('_')
         
         if 'blade' not in file.stem:
-            filename_parsed.append('')
-        
-        filenames_parsed.append([file, *filename_parsed])
-        del filename_parsed
-        
-    del files
+            filenames_parsed[i].append('')
             
-    filenames_parsed.sort(key= lambda x: (x[2],x[3],x[4])) # qty,turbine,blade
+    filenames_parsed.sort(key= lambda x: (x[1],x[2],x[3])) # qty,turbine,blade
     filenames_parsed = np.array(filenames_parsed)
     
-    quantities = np.unique(filenames_parsed[:,2])
-    turbines = np.unique(filenames_parsed[:,3])
-    blades = np.unique(filenames_parsed[:,4])
+    quantities = np.unique(filenames_parsed[:,1])
+    turbines = np.unique(filenames_parsed[:,2])
+    blades = np.unique(filenames_parsed[:,3])
+    blades = blades[blades != '']
     
-    for quantity in quantities:
-        logger.info(f'Plotting {quantity} for {casename}')
-        writefile = writedir / f'{quantity}.png'
+    ############################################################################
+    
+    for i, quantity in np.ndenumerate(quantities):
+        logger.debug(f'Plotting {quantity} for {casename}')
+        writefile = writedir / f'{casename}_{quantity}.png'
+        
+        if writefile.exists():
+            logger.warning(f'{writefile.name} already exists. '
+                           f'Skippping {quantity}.')
+            logger.warning('')
+            continue
         
         for turbine in turbines:
             
-            if quantity in 
-        
-        quantityfiles = filenames_parsed[]
-        
-        for file in files[np.logical_and(files[:,2]==quantity,
-                                            np.logical_or(files[:,4]=='',
-                                                        files[:,4]=='0'))]:
-            logger.debug(f'Reading {file[0].name}')
-            data = np.genfromtxt(file[0])
-            
             if quantity in const.TURBINE_QUANTITIES:
-                plt.plot(data[:,0],data[:,2],label=f'Turbine{file[3]}')
-                plt.plot(data[:,0],data[:,3],
-                            label=f'Turbine{file[3]},Average')
+                filename = readdir / f'{casename}_{quantity}_turbine{turbine}.gz'
                 
-                plt.title(f'{quantity}, {casename}')
-                plt.xlabel('Simulation Time')
-                plt.legend()
+                logger.debug(f'Reading {filename}')
+                try:
+                    data = np.genfromtxt(filename)
+                except FileNotFoundError:
+                    logger.warning(f'File {filename} not found, skipping.')
+                    continue
                 
-                fname = outputdir / f'{casename}_{quantity}.png'
-                logger.info(f'Saving file {fname.name}')   
-                plt.savefig(fname)
-                plt.close()
+                plt.plot(data[:,0], data[:,2], alpha=0.3,
+                         label=f'Turbine{turbine}')
                 
             elif quantity in const.BLADE_QUANTITIES:
-                plt.plot(data[:,0],data[:,2],
-                            label=f'Turbine{file[3]},Blade{file[4]},Root')
-                plt.plot(data[:,0],data[:,3],
-                            label=f'Turbine{file[3]},Blade{file[4]},Root,'
-                                f'Average')
-                plt.plot(data[:,0],data[:,-2],
-                            label=f'Turbine{file[3]},Blade{file[4]},Tip')
-                plt.plot(data[:,0],data[:,-1],
-                            label=f'Turbine{file[3]},Blade{file[4]},Tip,'
-                                f'Average')
+                filename = readdir / (f'{casename}_{quantity}_'
+                                        f'turbine{turbine}_blade{blades[0]}.gz')
                 
-                plt.title(f'{quantity}, {casename}')
-                plt.xlabel('Simulation Time')
-                plt.legend()
+                logger.debug(f'Reading {filename}')
+                try:
+                    data = np.genfromtxt(filename)
+                except FileNotFoundError:
+                    logger.warning(f'File {filename} not found, skipping.')
+                    continue
                 
-                fname = outputdir / f'{casename}_{quantity}.png'
-                logger.info(f'Saving file {fname.name}')   
-                plt.savefig(fname)
-                plt.close()
+                plt.plot(data[:,0], data[:,-1], alpha=0.3,
+                            label=f'Turbine{turbine},Blade{blades[0]},Tip')
                     
-                    
+        ########################################################################
+        
+        plt.title(f'{quantity}, {casename}')
+        plt.xlabel('Simulation Time')
+        plt.legend()
+        
+        writefile = writedir / f'{casename}_{quantity}.png'
+        logger.info(f'Saving file {writefile.name}')
+        logger.info(f'')
+        plt.savefig(writefile)
+        plt.close()
+        
+         
 if __name__ == "__main__":
     utils.configure_root_logger(level=LEVEL)
 
